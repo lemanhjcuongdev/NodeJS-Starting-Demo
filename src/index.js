dec33aa196fc6998e70ecd54ef07c6159e85a45c
cuonglm <cuong.dz.1806@gmail.com>
2023-12-14 09:57:11 +0700

    add bootstrap 5

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "public")));
+
 //HTTP Logger
 app.use(morgan("combined"));
 
-//Template Engine
+//Template Engine: set views
 app.engine(
     "hbs",
     engine({
@@ -22,6 +24,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
+//routes
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -29,6 +32,7 @@ app.get("/news", (req, res) => {
     res.render("news");
 });
 
+//listen port
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
